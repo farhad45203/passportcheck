1a07f25283ac0929df3a69cd1b739b9d3a6a3b18
farhad45203 <farhad45203@gmail.com>
2024-11-15 22:59:15 +0600

    A

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ function checkPassport(event) {
 
   // Display a popup if a match is found
   if (result) {
-    alert(`ID: ${result.id}\nName: ${result.name}`);
+    alert(`Congratulations\nID: ${result.id}\nName: ${result.name}`);
   } else {
     alert("No match found. Please check the passport number.");
   }
